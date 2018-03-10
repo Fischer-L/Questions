23b4da0a62d39e1fd362c79b86fcd605a3d66c37
foxbrush <fischer.json@gmail.com>
2018-03-10 16:23:30 +0800

    Done with Q2: Is Square

--- questions.js
+++ questions.js
@@ -12,3 +12,16 @@ export function reverseString(str) {
   return newStr;
 }
 
+/*
+ * Q2. Given a positive integer num, write a function which returns True if num is a perfect square else False. 
+ * Note: Do not use any built-in library function such as sqrt. 
+ * Example 1: 
+ * Input: 16 
+ * Returns: True 
+ */
+export function isSquare(n) {
+  for (let i = 1; n > 0; i += 2) {
+    n -= i;
+  }
+  return n === 0;
+}
--- questions.test.js
+++ questions.test.js
@@ -1,4 +1,7 @@
-import { reverseString } from './questions';
+import { 
+  reverseString,
+  isSquare
+} from './questions';
 
 describe("Q1: Reverse Strings", () => {
   it("should reverse strings", () => {
@@ -9,5 +12,17 @@ describe("Q1: Reverse Strings", () => {
       const reversed = s.split("").reverse().join("");
       expect(reverseString(s)).toBe(reversed);
     });
+  });
+});
+
+describe("Q2: Is Square", () => {
+  it("should judge square numbers to True", () => {
+    const nums = [ 0, 1, 2, 5, 17, 55, 85, 190 ];
+    nums.forEach(n => expect(isSquare(n ** 2)).toBe(true));
+  });
+
+  it("should judge non-square numbers to False", () => {
+    const nums = [ -1, -2, -5, 17, 55, 85, 190 ];
+    nums.forEach(n => expect(isSquare(n)).toBe(false));
   });
 });
